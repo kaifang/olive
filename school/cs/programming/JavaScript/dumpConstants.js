2f095bc942d284718eda35340de744eff4ff9256
Kai Fang <kayfang@yahoo.com>
2018-05-30 21:09:57 -0400

    Add files via upload

---
+++ school/cs/programming/JavaScript/dumpConstants.js
@@ -0,0 +1,20 @@
+export default {
+
+    FRONT: {
+        HTML: 'google/api/html.json',
+        CSS: 'google/api/css.json',
+        JS: 'google/api/javascript.json',
+        JSP: 'google/api/jsp.json'
+    },
+
+    BACK: {
+        C: 'ms/api/html.json',
+        JAVA: 'sun/api/html.json'
+    },
+
+    DB: {
+        SYBASE: 'sybase/api/sybase.json',
+        ORACLE: 'oracle/api/oracle.json',
+        MYSQL: 'open/api/mysql.json'
+    }
+};

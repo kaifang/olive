9edd576629a22346fe2fbf5f82c2df3239d299ac
Kai Fang <kayfang@yahoo.com>
2018-06-06 21:39:33 -0400

    Create feedbacks.js

---
+++ school/cs/programming/JavaScript/SlackAPI/data/feedbacks.js
@@ -0,0 +1,8 @@
+const feedbackSamples = [
+    'Is this note considered full as you list the \"what is\" question',
+    'I am shopping around for the best product.',
+    'I\'m well, and yourself?',
+    'Sorry for all of the questions'
+];
+
+module.exports.samples = feedbackSamples;
